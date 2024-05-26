009399547f5fdb114c66b65ff7110e470dccae9b
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-05-26 07:38:36 +0200

    auth setup with Clerk

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
-  return <p>Dashboard page (Protected)</p>;
+  return (
+    <div>
+      <p>Dashboard page (Protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default DashboardPage;
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
-  return <p>Landing Page (Unprotected)</p>;
+  return (
+    <div>
+      Landing Page (Unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
