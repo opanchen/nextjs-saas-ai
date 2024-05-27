df50894e15b43f8b6b9de6d9caefabc79549b387
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-05-27 19:22:10 +0200

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 const LandingPage = () => {
   return (
     <div>
       Landing Page (Unprotected)
-      <div>
+      <div className="flex items-center gap-2">
         <Link href="/sign-in">
           <Button>Login</Button>
         </Link>
